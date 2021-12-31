86a74a3b661f1af9b6c10c6e24b36e2842d3df66
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-31 16:50:17 +0200

    added EmployersAddForm in modules

--- employees_template/src/components/app/app.js
+++ employees_template/src/components/app/app.js
@@ -2,6 +2,7 @@ import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from "../employers-list/employers-list";
+import EmployersAddForm from "../employers-add-form/employers-add-form";
 
 import './app.css';
 
@@ -14,7 +15,9 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
+
             <EmployersList/>
+            <EmployersAddForm/>
         </div>
     );
 }
