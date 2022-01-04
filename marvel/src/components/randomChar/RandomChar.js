af9b254132cbb095f231946b45a6f6de41d39526
Fabarm <87820977+Fabarm@users.noreply.github.com>
2022-01-04 21:12:39 +0200

    fix code and applied method lifecycle component

--- marvel/src/components/randomChar/RandomChar.js
+++ marvel/src/components/randomChar/RandomChar.js
@@ -7,11 +7,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props){
-        super(props);
-        this.updateChar();
-    }
-
     state = {
         char: {},
         loading: true,
@@ -19,7 +14,16 @@ class RandomChar extends Component {
     }
     
     marvelService = new MarvelService();
-    
+
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 3000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+
     onCharLoaded = (char) => {
         this.setState({
             char,
