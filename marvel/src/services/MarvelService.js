9eb0fb6285a693c4f08f1281d2e3174b842c6ca0
Fabarm <87820977+Fabarm@users.noreply.github.com>
2022-01-07 23:40:01 +0200

    added pagination functional

--- marvel/src/services/MarvelService.js
+++ marvel/src/services/MarvelService.js
@@ -2,6 +2,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=366b4b8fa6686ca6e8989bfb836e22e4';
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -13,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
