2e5dd101a64c0f46e5548c57dbf9d35a4b23a5f8
Fabarm <87820977+Fabarm@users.noreply.github.com>
2022-01-17 20:25:31 +0200

    applied react-route

--- marvel/src/components/app/App.js
+++ marvel/src/components/app/App.js
@@ -1,49 +1,27 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
-
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
-import SingleComic from "../singleComic/SingleComic";
-import ComicsList from "../comicsList/ComicsList";
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id)
-    }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <ErrorBoundary>
-                <ComicsList/>
-            </ErrorBoundary>
-            {/*<main>*/}
-            {/*    <ErrorBoundary>*/}
-            {/*        <RandomChar/>*/}
-            {/*    </ErrorBoundary>*/}
-            {/*    <div className="char__content">*/}
-            {/*        <ErrorBoundary>*/}
-            {/*            <CharList onCharSelected={onCharSelected}/>*/}
-            {/*        </ErrorBoundary>*/}
-            {/*        <ErrorBoundary>*/}
-            {/*            <CharInfo charId={selectedChar}/>*/}
-            {/*        </ErrorBoundary>                    */}
-            {/*    </div>*/}
-            {/*    <img className="bg-decoration" src={decoration} alt="vision"/>*/}
-            {/*</main>*/}
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path='/'>
+                            <MainPage/>
+                        </Route>
+                        <Route exact path='/comics'>
+                            <ComicsPage/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
-}
-
-CharList.propTypes = {
-    onCharSelected: PropTypes.func.isRequired
 }
 
 export default App;
