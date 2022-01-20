ff42b0f978e3dd34fca525493c5b2e3b7ad18b97
Fabarm <87820977+Fabarm@users.noreply.github.com>
2022-01-20 16:33:46 +0200

    using React Transition Group

--- marvel/src/hooks/http.hook.js
+++ marvel/src/hooks/http.hook.js
@@ -1,13 +1,14 @@
 import { useState, useCallback } from "react";
 
 export const useHttp = () => {
-    const[loading, setLoading] = useState(false);
-    const[error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type' : 'aplication/json'}) => {
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+
         setLoading(true);
 
-        try{
+        try {
             const response = await fetch(url, {method, body, headers});
 
             if (!response.ok) {
@@ -17,9 +18,8 @@ export const useHttp = () => {
             const data = await response.json();
 
             setLoading(false);
-
             return data;
-        } catch(e){
+        } catch(e) {
             setLoading(false);
             setError(e.message);
             throw e;
@@ -28,6 +28,5 @@ export const useHttp = () => {
 
     const clearError = useCallback(() => setError(null), []);
 
-    return {loading, request, error, clearError};
+    return {loading, request, error, clearError}
 }
-
--- marvel/src/services/MarvelService.js
+++ marvel/src/services/MarvelService.js
@@ -12,25 +12,24 @@ const useMarvelService = () => {
     const getAllCharacters = async (offset = _baseOffset) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter);
-    };
+    }
 
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
-    };
+    }
 
     const getAllComics = async (offset = 0) => {
         const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformComics);
-    };
+    }
 
     const getComic = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComics(res.data.results[0]);
-    };
+    }
 
     const _transformCharacter = (char) => {
-
         return {
             id: char.id,
             name: char.name,
@@ -40,10 +39,9 @@ const useMarvelService = () => {
             wiki: char.urls[1].url,
             comics: char.comics.items
         }
-    };
+    }
 
     const _transformComics = (comics) => {
-
         return {
             id: comics.id,
             title: comics.title,
@@ -51,11 +49,11 @@ const useMarvelService = () => {
             pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
             thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
             language: comics.textObjects.language || 'en-us',
-            price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
+            price: comics.prices[0].price ? `${comics.prices[0].price}$` : 'not available'
         }
-    };
+    }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic};
+    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic}
 }
 
 export default useMarvelService;
