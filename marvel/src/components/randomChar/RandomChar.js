1c70b92e5b68eb67fa144df7a9b49e2fbde72b92
Fabarm <87820977+Fabarm@users.noreply.github.com>
2022-01-22 19:46:56 +0200

    used finite-state machine part 2

--- marvel/src/components/randomChar/RandomChar.js
+++ marvel/src/components/randomChar/RandomChar.js
@@ -1,46 +1,40 @@
-import { useState, useEffect } from 'react';
-import Spinner from "../spinner/Spinner";
-import ErrorMessage from "../errorMessage/ErrorMessage";
+import {useState, useEffect} from 'react';
 import useMarvelService from '../../services/MarvelService';
+import setContent from '../../utils/setContent';
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-const RandomChar= () => {
+const RandomChar = () => {
+
     const [char, setChar] = useState(null);
-    
-    const {loading, error, getCharacter, clearError} = useMarvelService();
+    const {getCharacter, clearError, process, setProcess} = useMarvelService();
 
     useEffect(() => {
         updateChar();
-        const timerId = setInterval(updateChar, 600000);
+        const timerId = setInterval(updateChar, 60000);
 
         return () => {
             clearInterval(timerId)
         }
-    }, []);  
+        // eslint-disable-next-line
+    }, [])
 
     const onCharLoaded = (char) => {
         setChar(char);
-    };
+    }
 
     const updateChar = () => {
         clearError();
-        const id = Math.floor(Math.random() * (1011400 - 1011000) +1011000);        
+        const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
         getCharacter(id)
-        .then(onCharLoaded);
-    };
-
-    
-    const errorMassage = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error || !char) ? <View char={char}/> : null;
+            .then(onCharLoaded)
+            .then(() => setProcess('confirmed'));
+    }
 
     return (
         <div className="randomchar">
-            {errorMassage}
-            {spinner}
-            {content}
+            {setContent(process, View, char)}
             <div className="randomchar__static">
                 <p className="randomchar__title">
                     Random character for today!<br/>
@@ -49,23 +43,25 @@ const RandomChar= () => {
                 <p className="randomchar__title">
                     Or choose another one
                 </p>
-                <button className="button button__main" onClick={updateChar}>
+                <button onClick={updateChar} className="button button__main">
                     <div className="inner">try it</div>
                 </button>
                 <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
             </div>
         </div>
-    )    
-    
+    )
 }
 
-const View = ({char}) => {
-    const {name, description, thumbnail, homepage, wiki} = char;
-    const im = thumbnail.includes("image_not_available") ? {"objectFit": "contain"} : {"objectFit": "conver"};    
+const View = ({data}) => {
+    const {name, description, thumbnail, homepage, wiki} = data;
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} style={im} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
